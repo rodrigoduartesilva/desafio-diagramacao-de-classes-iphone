9d86664c1b5db0316d1d32355710a207c7c1b5d1
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2024-05-30 16:57:44 -0300

    feat: classe ipod implementada

--- src/br/com/duarte/desafioumliphone/main/Main.java
+++ src/br/com/duarte/desafioumliphone/main/Main.java
@@ -1,3 +1,5 @@
+package br.com.duarte.desafioumliphone.main;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/br/com/duarte/desafioumliphone/model/Ipod.java
@@ -0,0 +1,38 @@
+package br.com.duarte.desafioumliphone.model;
+
+public class Ipod extends Iphone{
+    private String musica;
+    private String artista;
+
+    public String getMusica() {
+        return musica;
+    }
+
+    public void setMusica(String musica) {
+        this.musica = musica;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void tocarMusica(String musica) {
+        System.out.println("Música tocando");
+    }
+
+    public void selecionarArtista(String artista) {
+        System.out.println("Artista selecionado!");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada!");
+    }
+
+    public void pausarMusica(String musica) {
+        System.out.println("Música pausada!");
+    }
+}
